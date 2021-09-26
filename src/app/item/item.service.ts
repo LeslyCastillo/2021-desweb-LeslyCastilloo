14c3b0ca30406e4c156278529e9baea4ef4e4758
LeslyCastillo31 <leslymaribel31@gmail.com>
2021-09-25 23:15:23 -0600

    Se agrego api-key para restablecer limites de solicitudes

--- src/app/item/item.service.ts
+++ src/app/item/item.service.ts
@@ -13,7 +13,7 @@ export class ItemService {
   }
 
   getItems(){
-    return this.http.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=DEMO_KEY');
+    return this.http.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=MvjTNWfygmxMXo9zBpwpBSMkqw15ADyyKqfElcHD');
   }
 
  
